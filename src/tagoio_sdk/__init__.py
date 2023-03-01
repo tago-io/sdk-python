16b5f36722d9c158e00b377c4be7984af426437a
Felipe Lima <felipe@ferreiradelima.com>
2023-03-01 14:59:08 -0500

    add dynamic version and add aiohttp as extra

--- src/tagoio_sdk/__init__.py
+++ src/tagoio_sdk/__init__.py
@@ -1,4 +1,8 @@
-__version__ = "4.0.7"
+import toml
+
+with open("pyproject.toml") as f:
+    pyproject = toml.load(f)
+    __version__ = pyproject["tool"]["poetry"]["version"]
 
 from .modules.Account.Account import Account
 from .modules.Analysis.Analysis import Analysis
