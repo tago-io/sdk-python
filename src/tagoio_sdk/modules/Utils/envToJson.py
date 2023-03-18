61a95c0b1f02ac0e61fd4ce84eefaa5f2d7a3a04
Mateus Cardoso <mateus.informatica44@gmail.com>
2023-03-17 21:22:52 -0300

    Fixed envToJson method and added unit test for this method.

--- src/tagoio_sdk/modules/Utils/envToJson.py
+++ src/tagoio_sdk/modules/Utils/envToJson.py
@@ -1,12 +1,6 @@
 from functools import reduce
-from typing import Any
 
 from tagoio_sdk.modules.Analysis.Analysis_Type import AnalysisEnvironment
-
-
-def reduceFunction(pv: Any, cv: Any) -> Any:
-    pv[cv["key"]] = cv["value"]
-    return pv
 
 
 def envToJson(environment: list[AnalysisEnvironment]) -> AnalysisEnvironment:
@@ -18,4 +12,6 @@ def envToJson(environment: list[AnalysisEnvironment]) -> AnalysisEnvironment:
     :param AnalysisEnvironment environment Array of environment
     items from TagoIO
     """
-    return reduce(reduceFunction, environment)
+    return reduce(
+        lambda key, value: {**key, value["key"]: value["value"]}, environment, {}
+    )
---
+++ tests/Utils/test_env_to_json.py
@@ -0,0 +1,20 @@
+from tagoio_sdk.modules.Utils.envToJson import envToJson
+
+
+CONTEXT_ENVIRONMENT = [
+    {"key": "account_token", "value": "test_token"},
+    {"key": "device_token", "value": "test_token"},
+    {"key": "input", "value": "95"},
+    {"key": "output", "value": "95"},
+]
+
+
+def test_env_to_json():
+    """Test envToJson function."""
+    expected = {
+        "account_token": "test_token",
+        "device_token": "test_token",
+        "input": "95",
+        "output": "95",
+    }
+    assert envToJson(CONTEXT_ENVIRONMENT) == expected
