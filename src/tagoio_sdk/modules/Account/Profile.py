a921823052bf9ead18356ee00443cfafdf1286a2
Mateus Cardoso <mateus.informatica44@gmail.com>
2023-03-17 21:27:20 -0300

    Added method tokenList in profile and added all unit test for the class Profile.

--- src/tagoio_sdk/modules/Account/Profile.py
+++ src/tagoio_sdk/modules/Account/Profile.py
@@ -1,11 +1,12 @@
-from tagoio_sdk.common.Common_Type import GenericID
+from typing import List, Optional
+from tagoio_sdk.common.Common_Type import GenericID, Query, TokenDataList
 from tagoio_sdk.common.tagoio_module import TagoIOModule
 from tagoio_sdk.modules.Account.Profile_Type import (
     ProfileInfo,
     ProfileListInfo,
     ProfileSummary,
 )
-from tagoio_sdk.modules.Utils.dateParser import dateParser
+from tagoio_sdk.modules.Utils.dateParser import dateParser, dateParserList
 
 
 class Profile(TagoIOModule):
@@ -15,7 +16,7 @@ class Profile(TagoIOModule):
     using functions like create, edit and delete.
     """
 
-    def info(self, profileID: GenericID) -> list[ProfileInfo]:
+    def info(self, profileID: GenericID) -> ProfileInfo:
         """
         Get Profile info
         :param: profileID Profile identification
@@ -27,8 +28,8 @@ class Profile(TagoIOModule):
             }
         )
 
-        if result.info:
-            result.info = dateParser(result.info, ["created_at", "updated_at"])
+        if result.get("info"):
+            result["info"] = dateParser(result["info"], ["created_at", "updated_at"])
         return result
 
     def list(self) -> list[ProfileListInfo]:
@@ -54,4 +55,39 @@ class Profile(TagoIOModule):
                 "method": "GET",
             }
         )
+        return result
+
+    def tokenList(
+        self, profileID: GenericID, queryObj: Optional[Query] = {}
+    ) -> List[TokenDataList]:
+        """
+        Lists all the tokens in your account
+        :param: profileID Profile identification
+        """
+
+        if "orderBy" in queryObj:
+            firstArgument = queryObj["orderBy"][0]
+            secondArgument = queryObj["orderBy"][1]
+            orderBy = f"{firstArgument},{secondArgument}"
+        else:
+            orderBy = "created_at, asc"
+
+        result = self.doRequest(
+            {
+                "path": f"/profile/{profileID}/token",
+                "method": "GET",
+                "params": {
+                    "page": queryObj.get("page") or 1,
+                    "fields": queryObj.get("fields") or ["name", "token", "permission"],
+                    "filter": queryObj.get("filter") or {},
+                    "amount": queryObj.get("amount") or 20,
+                    "orderBy": orderBy,
+                },
+            }
+        )
+
+        result = dateParserList(
+            result, ["last_authorization", "expire_time", "created_at"]
+        )
+
         return result
