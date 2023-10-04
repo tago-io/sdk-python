da54da25037491439cedb0dea5652825817143ec
mateuscardosodeveloper <mateus.informatica44@gmail.com>
2023-10-04 17:51:31 -0300

    File with all type annotations

---
+++ src/tagoio_sdk/types.py
@@ -0,0 +1,16 @@
+# flake8: noqa: F401, F403
+from  .common.Common_Type import *
+
+from .modules.Resources.Account_Types import *
+from .modules.Resources.Buckets_Type import *
+from .modules.Resources.Dashboards_Type import *
+from .modules.Resources.Device_Type import *
+from .modules.Resources.Notification_Type import *
+from .modules.Resources.Profile_Type import *
+from .modules.Resources.Run_Type import *
+from .modules.Resources.Billing_Type import *
+from .modules.Resources.IntegrationNetworkType import *
+
+from .modules.Device.Device_Type import *
+from .modules.Analysis.Analysis_Type import *
+from .modules.Utils.utilsType import *
